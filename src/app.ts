8e3c13a7cb8f377485761cb6c7e32d6d88e4307f
Tuomas Valkeapää <tuomas.valkeapaa@gmail.com>
2022-05-20 15:16:17 +0300

    Added a required header for all requests

--- src/app.ts
+++ src/app.ts
@@ -22,39 +22,61 @@ let connection: Connection | undefined = undefined;
 
 // Returns all putt results
 app.get("/putt-results", async (request, response) => {
-  if (!connection) {
-    connection = await createConnection();
+  if (checkHeaders(request)) {
+    if (!connection) {
+      connection = await createConnection();
+    }
+    const allPuttResults = await queryAllPuttResults(connection);
+    response.end(JSON.stringify(allPuttResults));
   }
-  const allPuttResults = await queryAllPuttResults(connection);
-  response.end(JSON.stringify(allPuttResults));
+  response.status(401);
+  response.end();
 });
 
 // Creates a new row to the db to puttResult table
 app.post("/mark-putt", async (request, response) => {
-  if (!connection) {
-    connection = await createConnection();
-  }
-  const puttData: newPuttInsert | undefined = request.body;
-  if (puttData) {
-    console.log(
-      "Inserting a new row into puttResult table: " + JSON.stringify(puttData)
-    );
-    const insertResult = await insertNewPuttResult(connection, puttData);
-    response.end(JSON.stringify(insertResult));
-  } else {
-    response.end("Error marking a putt: No putt data in the request body");
+  if (checkHeaders(request)) {
+    if (!connection) {
+      connection = await createConnection();
+    }
+    const puttData: newPuttInsert | undefined = request.body;
+    if (puttData) {
+      console.log(
+        "Inserting a new row into puttResult table: " + JSON.stringify(puttData)
+      );
+      const insertResult = await insertNewPuttResult(connection, puttData);
+      response.end(JSON.stringify(insertResult));
+    } else {
+      response.end("Error marking a putt: No putt data in the request body");
+    }
+    response.status(401);
+    response.end();
   }
 });
 
 // Undo the last putt result that is not undone. Returns the undone putt or true, if no there is no putt to to undo.
 app.put("/undo-putt", async (request, response) => {
-  if (!connection) {
-    connection = await createConnection();
+  if (checkHeaders(request)) {
+    if (!connection) {
+      connection = await createConnection();
+    }
+    const undoResult = await undoLastPutt(connection);
+    response.end(JSON.stringify(undoResult));
   }
-  const undoResult = await undoLastPutt(connection);
-  response.end(JSON.stringify(undoResult));
+  response.status(401);
+  response.end();
 });
 
 app.listen(port, () => {
   console.log("Putt stats API app listening at http://%s:%s", hostname, port);
 });
+
+// Need certain headers for all requests. This is done as a very low level security measure to prevent unwanted SQL calls to GCP.
+const checkHeaders = (request: any): boolean => {
+  const throwDataHeaderValue = request?.headers?.throwdata;
+  if (!throwDataHeaderValue) {
+    console.log("No required header");
+    return false;
+  }
+  return true;
+};

d1694a444a53dccf06bac4b3ea387cc2169504cd
Tuomas Valkeapää <tuomas.valkeapaa@gmail.com>
2022-05-27 15:14:13 +0300

    Added throwdata as an accepted header for CORS

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.use(function (req, res, next) {
   );
   res.header(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With"
+    "Content-Type, Authorization, Content-Length, X-Requested-With, throwdata"
   );
   // allow preflight
   if (req.method === "OPTIONS") {
